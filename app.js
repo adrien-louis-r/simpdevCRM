b1b0033b0286b0070ebf429c18d5e91bb0a0be9f
Adrien Louis-Rossignol <adrien.louis.r@gmail.com>
2020-06-26 16:26:27 +0200

    Configure simple gql server

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => 'Hello world!',
+};
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -13,6 +25,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  }),
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
